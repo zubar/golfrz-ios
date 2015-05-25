febbc1b41c4899afbd9c2fd501515c2b03a25bd5
Zubair Muhammad <muhammad.zubair@tkxel.com>
2015-05-25 11:42:07 +0500

    [GOLFRZ-121] add file alertConstants.h

---
+++ GolfersProject/Alert-Constants.h
@@ -0,0 +1,16 @@
+//
+//  Alert-Constants.h
+//  GolfersProject
+//
+//  Created by Zubair on 5/25/15.
+//  Copyright (c) 2015 Abdullah Saeed. All rights reserved.
+//
+
+#ifndef GolfersProject_Alert_Constants_h
+#define GolfersProject_Alert_Constants_h
+
+
+static NSString * const kBaseImageUrl = @"https://powerful-plains-9156.herokuapp.com/";
+
+
+#endif
