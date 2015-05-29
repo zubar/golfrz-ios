7dd8c44ab1370e01851701b6b37f87dfd38072a2
Abdullah Saeed <abdullah.saeed@tkxel.com>
2015-05-29 16:08:54 +0500

    [GOLFRZ-79] added method stubs for player settings

--- GolfersProject/ViewController/PlayerSettingsViewController/PlayerSettingsMainViewController.h
+++ GolfersProject/ViewController/PlayerSettingsViewController/PlayerSettingsMainViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PlayerSettingsMainViewController : UIViewController
+@interface PlayerSettingsMainViewController : UIViewController<UITextFieldDelegate>
+@property (strong, nonatomic) IBOutlet UILabel *lblEditProfile;
+@property (strong, nonatomic) IBOutlet UIImageView *imgUserPic;
+@property (strong, nonatomic) IBOutlet UITextField *txtFirstName;
+@property (strong, nonatomic) IBOutlet UITextField *txtLastName;
+@property (strong, nonatomic) IBOutlet UITextField *txtEmailAddress;
+@property (strong, nonatomic) IBOutlet UILabel *lblResetPassword;
+@property (strong, nonatomic) IBOutlet UIImageView *lblLogout;
 
 @end
--- GolfersProject/ViewController/PlayerSettingsViewController/PlayerSettingsMainViewController.m
+++ GolfersProject/ViewController/PlayerSettingsViewController/PlayerSettingsMainViewController.m
@@ -16,12 +16,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self addGestureToEditProfile];
+    [self addGestureToLogout];
+    [self addGestureToResetPassword];
+    
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(void) addGestureToEditProfile{
+    UITapGestureRecognizer* gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(editProfileTapped)];
+    // if labelView is not set userInteractionEnabled, you must do so
+    [self.lblEditProfile setUserInteractionEnabled:YES];
+    [self.lblEditProfile addGestureRecognizer:gesture];
+}
+
+
+-(void) addGestureToResetPassword{
+    UITapGestureRecognizer* gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(resetPasswordTapped)];
+    // if labelView is not set userInteractionEnabled, you must do so
+    [self.lblResetPassword setUserInteractionEnabled:YES];
+    [self.lblResetPassword addGestureRecognizer:gesture];
+}
+
+
+-(void) addGestureToLogout{
+    UITapGestureRecognizer* gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(logoutTapped)];
+    // if labelView is not set userInteractionEnabled, you must do so
+    [self.lblLogout setUserInteractionEnabled:YES];
+    [self.lblLogout addGestureRecognizer:gesture];
+}
+
+
+- (void) editProfileTapped{
+    
+}
+
+- (void) resetPasswordTapped{
+    
+}
+
+- (void) logoutTapped {
+    
 }
 
 /*
@@ -33,5 +74,13 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma TextFieldMethods
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return NO;
+}
+
 
 @end
