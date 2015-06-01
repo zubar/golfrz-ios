8144e3ab74c9318fac83ae8796d81fc0acb18ca1
Abdullah Saeed <abdullah.saeed@tkxel.com>
2015-06-01 11:58:14 +0500

    created splash screen view controller

---
+++ GolfersProject/ViewController/SplashScreenViewController/SplashScreenViewController.m
@@ -0,0 +1,37 @@
+//
+//  SplashScreenViewController.m
+//  GolfersProject
+//
+//  Created by Abdullah Saeed on 6/1/15.
+//  Copyright (c) 2015 Abdullah Saeed. All rights reserved.
+//
+
+#import "SplashScreenViewController.h"
+
+@interface SplashScreenViewController ()
+
+@end
+
+@implementation SplashScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
