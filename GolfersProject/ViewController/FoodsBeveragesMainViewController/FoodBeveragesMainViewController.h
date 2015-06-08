bc51dc144f94f93e2179e7ee8581e27edc8d0a19
Abdullah Saeed <abdullah.saeed@tkxel.com>
2015-06-08 11:44:30 +0500

    added Food Bev VC

---
+++ GolfersProject/ViewController/FoodsBeveragesMainViewController/FoodBeveragesMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoodBeveragesMainViewController.h
+//  GolfersProject
+//
+//  Created by Abdullah Saeed on 6/8/15.
+//  Copyright (c) 2015 Abdullah Saeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodBeveragesMainViewController : UIViewController
+
+@end
