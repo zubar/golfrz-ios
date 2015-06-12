4b7f3ba241c20532fa0823486b8e5ada0898a709
Abdullah Saeed <abdullah.saeed@tkxel.com>
2015-06-12 18:27:56 +0500

    add VC for food and bev item details

---
+++ GolfersProject/ViewController/FoodBevItemDetailViewController/FoodBevItemDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoodBevItemDetailViewController.h
+//  GolfersProject
+//
+//  Created by Abdullah Saeed on 6/12/15.
+//  Copyright (c) 2015 Abdullah Saeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodBevItemDetailViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+
+@end
