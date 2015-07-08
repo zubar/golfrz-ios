d37c8c6c787761ac656b154281eac5f551739535
Abdullah Saeed <abdullah.saeed@tkxel.com>
2015-07-08 17:48:34 +0500

    Completed design of tee-times VC

---
+++ GolfersProject/Views/TeeTimeBookingCell/TeeTimeBookingCell.m
@@ -0,0 +1,25 @@
+//
+//  TeeTimeBookingCell.m
+//  GolfersProject
+//
+//  Created by Abdullah Saeed on 7/8/15.
+//  Copyright (c) 2015 Abdullah Saeed. All rights reserved.
+//
+
+#import "TeeTimeBookingCell.h"
+
+@implementation TeeTimeBookingCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (IBAction)btnBookTapped:(UIButton *)sender {
+}
+@end
