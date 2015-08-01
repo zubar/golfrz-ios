63b08cc8c9102ef00a6cb12217846f6ca49dfda9
Abdullah Saeed <abdullah.saeed@tkxel.com>
2015-08-01 17:24:04 +0500

    scorecard background handling

--- GolfersProject/Services/ViewControllers/ScoreBoardViewController/ScoreBoardViewController.m
+++ GolfersProject/Services/ViewControllers/ScoreBoardViewController/ScoreBoardViewController.m
@@ -18,65 +18,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    //_parentScrollVIew.contentSize = CGSizeMake(320, 700);
-//    _rightCollectionView.contentSize = CGSizeMake(320, 400);
- //   _leftCollectionView.contentSize = CGSizeMake(320, 700);
     [ScoreBoardManager sharedScoreBoardManager].numberOfItems = 18;
     [ScoreBoardManager sharedScoreBoardManager].numberOfSections = 100;
 }
 
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-    if (collectionView == _leftCollectionView)
-    {
-     
-        return 0;
-    }
-    return 18;
+   
+    return 12;
     
 }
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
-    if (collectionView == _leftCollectionView)
-    {
-        return 0;
-    }
-    return 100;
+    
+    return 18;
 }
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    
-    if (collectionView == _leftCollectionView) {
-        
-        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"ParentCell" forIndexPath:indexPath];
-        if (cell == nil)
-        {
-            cell = [[UICollectionViewCell alloc]init];
-        }
-        ScoreBoardParentCell *customCell = (ScoreBoardParentCell *)cell;
-//        if (indexPath.section == 0) {
-//            //customCell.backgroundColor = [UIColor darkGrayColor];
-//        }else{
-//            //customCell.backgroundColor = [UIColor colorWithRed:63/255 green:63/255 blue:65/255 alpha:1];
-//            customCell.backgroundColor = [UIColor darkGrayColor];
-//        }
-        return customCell;
-    }
     
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"rightParentCell" forIndexPath:indexPath];
     if (cell == nil)
     {
         cell = [[UICollectionViewCell alloc]init];
     }
-//    ScoreBoardParentCell *customCell = (ScoreBoardParentCell *)cell;
-//    if (indexPath.section == 0) {
-//        //customCell.backgroundColor = [UIColor darkGrayColor];
-//    }else{
-//        //customCell.backgroundColor = [UIColor colorWithRed:63/255 green:63/255 blue:65/255 alpha:1];
-//        customCell.backgroundColor = [UIColor darkGrayColor];
-//    }
+    
+    if (indexPath.row > 5)
+    {
+        NSLog(@"Index-Path:%@ row:%ld  item:%ld", indexPath, (long)indexPath.row, (long)indexPath.item);
+        cell.backgroundColor = [UIColor lightGrayColor];
+    }else{
+        cell.backgroundColor = [UIColor blackColor];
+    }
+
     return cell;
+}
+
+-(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    NSLog(@"Clicked");
 }
 /*
 #pragma mark - Navigation
