a735a33b64499d9cf81c116ad23a93ed1376eeed
Abdullah Saeed <abdullah.saeed@tkxel.com>
2015-08-25 12:47:18 +0500

    fixed add guest VC, added weather date label on weather view

--- GolfersProject/Services/ViewControllers/AddGuestViewController/AddGuestViewController.h
+++ GolfersProject/Services/ViewControllers/AddGuestViewController/AddGuestViewController.h
@@ -22,5 +22,6 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imgViewBackground;
 - (IBAction)btnSelectTeeBoxTapped:(UIButton *)sender;
 - (IBAction)btnAddGuestTapped:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UILabel *lblTeeBox;
 
 @end
