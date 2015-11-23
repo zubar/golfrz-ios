ee38606c0d8201de12dd7fe17ecb638b2c24c037
Zubair Muhammad <muhammad.zubair@tkxel.com>
2015-11-23 19:16:21 +0500

    Removed un-used variable

--- HelloJinkin/ViewController.m
+++ HelloJinkin/ViewController.m
@@ -18,7 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSMutableArray * array =[[NSMutableArray alloc]initWithCapacity:3];
     
 }
 
