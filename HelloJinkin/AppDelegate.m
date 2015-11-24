b34382b04c309038baeaf7222f309d00ff709ea1
Zubair Muhammad <muhammad.zubair@tkxel.com>
2015-11-24 16:08:35 +0500

    added UTC method

--- HelloJinkin/AppDelegate.m
+++ HelloJinkin/AppDelegate.m
@@ -42,4 +42,8 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+
+- (NSDate *)toUCTTimeZone:(NSDate *)date{
+    return [NSDate date];
+}
 @end
--- HelloJinkin/ViewController.m
+++ HelloJinkin/ViewController.m
@@ -26,4 +26,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSDate *)toUCTTimeZone:(NSDate *)date{
+    return [NSDate date];
+}
 @end
